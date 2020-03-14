762d5452ee0b231d59e98e42c1663d52aba07738
Oktay Şenkan <oktay.senkan@gmail.com>
2020-03-14 19:45:35 +0300

    minor refactoring

--- src/useCountries.ts
+++ src/useCountries.ts
@@ -4,8 +4,8 @@ import locales, { Country, Language } from "./locales";
 const useCountries = () => {
   const countries = locales.countries;
   const languages = locales.languages;
-  const [country, setCountry] = useState<Country>(locales.countries[0]);
-  const [language, setLanguage] = useState<Language>(locales.languages[0]);
+  const [country, setCountry] = useState<Country>(countries[0]);
+  const [language, setLanguage] = useState<Language>(languages[0]);
 
   return { countries, languages, country, setCountry, language, setLanguage };
 };
